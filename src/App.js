eb16951faba35be92ad22c60d81a1275e33b77a6
Kevin <kevin.j.dimarco@gmail.com>
2020-10-22 18:09:11 -0700

    Adding App key and ID vars

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 
 const App = () => {
 
+  const APP_ID = '0f28e298';
+  const APP_KEY = 'b56e3ba36e59352bf8f05e481a550c1a';
+
   return (
     <div className="App">
       <h1>Hello React</h1>
