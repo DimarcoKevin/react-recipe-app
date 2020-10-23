a4e38047baa3351bf23f63980eeca8db70c52d3d
Kevin <kevin.j.dimarco@gmail.com>
2020-10-22 22:22:43 -0700

    Adding ingredients into recipe

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
-const Recipe = ({title, calories, image}) => {
+const Recipe = ({title, calories, ingredients, image}) => {
     calories = Math.round(calories);
     return (
         <div>
             <h1>{title}</h1>
             <p>{calories} calories</p>
+            <ul>
+                {ingredients.map(ingredient => (
+                    <li>{ingredient.text}</li>
+                ))}
+            </ul>
             <img src={image} alt={title + " image"}></img>
         </div>
     );
