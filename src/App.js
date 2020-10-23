1403b39e9190b72659ace4c3867be9684952640e
Kevin <kevin.j.dimarco@gmail.com>
2020-10-22 21:21:55 -0700

    Adding params to Recipe component

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ const App = () => {
   const getRecipes = async () => {
     const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
+    console.log(data.hits);
     setRecipes(data.hits);
   };
 
@@ -28,7 +29,11 @@ const App = () => {
         </button>
       </form>
       {recipes.map(recipe => (
-        <Recipe></Recipe>
+        <Recipe 
+          title={recipe.recipe.label} 
+          calories={recipe.recipe.calories} 
+          image={recipe.recipe.image}
+        />
       ))};
     </div>
   );
