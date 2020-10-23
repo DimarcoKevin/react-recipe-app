d6777f13d45eb545d67af42d0df58d8a0bde67b8
Kevin <kevin.j.dimarco@gmail.com>
2020-10-22 19:39:48 -0700

    Removing temporary counter data and adding async/await function

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ const App = () => {
   const APP_ID = '0f28e298';
   const APP_KEY = 'b56e3ba36e59352bf8f05e481a550c1a';
 
-  const example = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
-
-  const [counter, setCounter] = useState(0);
-
   useEffect(() => {
-    console.log('Effect has been run');
-  });
+    
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = response.json();
+    console.log(data);
+  };
 
   return (
     <div className="App">
@@ -22,7 +24,6 @@ const App = () => {
           Search
         </button>
       </form>
-      <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
     </div>
   );
 };
