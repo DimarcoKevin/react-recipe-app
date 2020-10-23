bf944eed58ba11eee326fa8ea3b34d7fc0eb2066
Kevin <kevin.j.dimarco@gmail.com>
2020-10-22 20:33:31 -0700

    No longer logging data, sending it to a setRecipes state

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ const App = () => {
   const APP_ID = '0f28e298';
   const APP_KEY = 'b56e3ba36e59352bf8f05e481a550c1a';
 
+  const [recipes, setRecipes] = useState([]);
+
   useEffect(() => {
-    
+    getRecipes();
   }, []);
 
   const getRecipes = async () => {
     const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
-    console.log(data);
+    setRecipes(data.hits);
   };
 
   return (
